7fb40f9fa09d1f1ebd2bc99a1d6188ec78cf0e13
hoto17296 <mail@hoto.me>
2017-11-04 08:27:36 +0000

    save image to file

---
+++ save_image_to_file.py
@@ -0,0 +1,20 @@
+import MySQLdb
+import MySQLdb.cursors
+
+dsn = {
+    'host': '192.168.33.13',
+    'port': 3306,
+    'user': 'isucon',
+    'password': 'isucon',
+    'database': 'isubata',
+    'cursorclass': MySQLdb.cursors.SSCursor,
+}
+
+conn = MySQLdb.connect(**dsn)
+cur = conn.cursor()
+cur.execute('SELECT DISTINCT name, data FROM image')
+for name, data in cur:
+    with open('/home/isucon/image/{}'.format(name), 'wb') as f:
+        f.write(data)
+cur.close()
+conn.close()
